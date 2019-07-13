f5c77dc5aa302140bb4d74fccd57ac4bd29f9f6d
Jessica Pena <penajessica123@yahoo.com>
2019-07-13 11:45:03 -0500

    added the first axios solving the next

--- src/index.js
+++ src/index.js
@@ -9,47 +9,70 @@ const api_key = require('../config')
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key.API_KEY}`
   // code here
-  axios.get(url)
-  .then(function(response){
-    return axios.get(url + response.data.results);
-  })
-  .catch(function(error){
-    return (error);
-  })
-  .finally(function(){
+  
+    axios.get(url).then(function(response) {
+      console.log(response.data)
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+    return axios(url).then(response => response)
+  }
+  discoverMovie()
 
-  })
-}
-discoverMovie()
-
-// const getMovieById = (id) => {
-//   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key.API_KEY}`
-//   // code here
-//   axios.get(url, {
-//     params: {
-//       ID: 500
-//     }
-//     .then(function(response){
-//       return response.data.original_title
-//     })
-//     .catch(function(error){
-//       return error
-//     })
-//     .finally(function(){
-//     })
+//   axios.get(url)
+//   .then(function(response){
+//     return axios.get(url + response.data.results);
+//   })
+//   .catch(function(error){
+//     return (error);
+//   })
+//   .finally(function(){
+//   })
 // }
+// discoverMovie()
+
+const getMovieById = (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key.API_KEY}`
+  // code here
+  axios.get(url).then(function(id) {
+    params: {
+      ID: 500
+    }
+    
+    // console.log(response.data)
+    return axios(url).then(original_title => original_title)
+    
+  })
+  console.log(id)
+  // .catch(function(error){
+  //   return error
+  // })
+  // axios.get(url, {
+  //   params: {
+  //     ID: 500
+  //   }
+    // .then(function(response){
+    //   return response.data.original_title
+    // })
+    // .catch(function(error){
+    //   return error
+    // })
+    // .finally(function(){
+    // })
+}
 getMovieById(500)
 
-const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
-  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key.API_KEY}`
-  // code here
-}
+// const getMovieByIdFailure = () => {
+//   const fakeId = 1 // FAKE ID HERE
+//   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key.API_KEY}`
+//   // code here
+// }
 
 
 
 module.exports = {
   discoverMovie,
   getMovieById,
-  getMovieByIdFailure
+  // getMovieByIdFailure
 }
