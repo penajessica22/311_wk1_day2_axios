be49df171e1d405fd412cd8821e3b0c5445ab223
Jessica Pena <penajessica123@yahoo.com>
2019-07-20 09:48:59 -0500

    finished the axios assignment

--- src/index.js
+++ src/index.js
@@ -1,78 +1,45 @@
 // import axios here
-const axios = require('axios');
-const api_key = require('../config')
-// Please note that it is normally not considered best practice to commit 
-// api keys to github as it presents a security risk. It is done here only 
+const axios = require("axios");
+const api_key = require("../config");
+// Please note that it is normally not considered best practice to commit
+// api keys to github as it presents a security risk. It is done here only
 // for practice purposes as we are sharing the same account
 
-
 const discoverMovie = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key.API_KEY}`
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key.API_KEY}`;
   // code here
-  
-    axios.get(url).then(function(response) {
-      console.log(response.data)
-    })
-    .catch(function(error){
-      console.log(error)
-    })
-    return axios(url).then(response => response)
-  }
-  discoverMovie()
 
-//   axios.get(url)
-//   .then(function(response){
-//     return axios.get(url + response.data.results);
-//   })
-//   .catch(function(error){
-//     return (error);
-//   })
-//   .finally(function(){
-//   })
-// }
-// discoverMovie()
+  axios.get(url).then(function(response) {
+      // console.log(response.data)
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  return axios(url).then(response => response);
+};
+discoverMovie()
 
-const getMovieById = (id) => {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key.API_KEY}`
-  // code here
-  axios.get(url).then(function(data) {
-    // params: {
-    //   ID: 500
-    // }
-    
-    // console.log(response.data)
-    // return axios(url).then(original_title => original_title)
-    
-  })
-  console.log(id)
-  // .catch(function(error){
-  //   return error
+const getMovieById = id => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key.API_KEY}`;
+  return axios(url).then(response => response.data);
+  // axios.get(url).then(function(result) {
+  //   return result.data.id
   // })
-  // axios.get(url, {
-  //   params: {
-  //     ID: 500
-  //   }
-    // .then(function(response){
-    //   return response.data.original_title
-    // })
-    // .catch(function(error){
-    //   return error
-    // })
-    // .finally(function(){
-    // })
-}
-getMovieById(500)
+};
+getMovieById(500);
 
-// const getMovieByIdFailure = () => {
-//   const fakeId = 1 // FAKE ID HERE
-//   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key.API_KEY}`
-//   // code here
-// }
-
+const getMovieByIdFailure = () => {
+  const fakeId = 5783 
+  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key.API_KEY}`
+  // code here
+  return axios(url).then(response => response.status)
+  .catch(err => err.response.status)
+  
+  };
 
 
 module.exports = {
   discoverMovie,
   getMovieById,
-  // getMovieByIdFailure
-}
+  getMovieByIdFailure
+};
